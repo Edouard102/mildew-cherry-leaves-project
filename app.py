3e12ba9f6a66860ea4b5a5380765c59650548da1
Edouard102 Trentesaux <edouard102@gmx.fr>
2024-05-08 14:33:02 +0000

    add folder and content to src, and content to page mildew detector

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ from app_pages.multipage import MultiPage
 # load pages scripts
 from app_pages.page_summary import page_summary_body
 from app_pages.page_leaves_visualizer import page_leaves_visualizer_body
-
+from app_pages.page_mildew_detector import page_mildew_detector_body
 
 app = MultiPage(app_name="Powdery Mildew Diease Detector")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
 app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Leaves Visualiser", page_leaves_visualizer_body)
+app.add_page("Mildew dectector", page_mildew_detector_body)
 
 app.run()  # Run the app
